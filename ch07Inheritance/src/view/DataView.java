431ff402e9793d8b178722fb24870ebedb269e79
401PC-D5\hong <johnmor78@gmail.com>
2018-02-14 13:30:32 +0900

    no message

---
+++ ch07Inheritance/src/view/DataView.java
@@ -0,0 +1,13 @@
+package view;
+
+import a.Print;
+
+public class DataView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		a.Print p = new a.Print();
+		Print p = new Print();
+	}
+
+}
