8c57ed57d31d8cf469f5b3fb6a7bd889bf2148cf
401PC-D5\hong <johnmor78@gmail.com>
2018-02-27 18:48:44 +0900

    no message

---
+++ JavaTeamProject/src/boardController/BoardController.java
@@ -0,0 +1,15 @@
+package boardController;
+
+public class BoardController {
+	
+	public void selectMenu() {
+		System.out.println(""
+				+ ""
+		);
+	}
+
+	
+}
+
+
+
---
+++ JavaTeamProject/src/boardDTO/BoardDTO.java
@@ -0,0 +1,52 @@
+package boardDTO;
+
+public class BoardDTO {
+	int no;
+	String title;
+	String content;
+	String writer;
+	String writeDate;
+	int hit;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	@Override
+	public String toString() {
+		return "BoardDTO [no=" + no + ", title=" + title + ", content=" + content + ", writer=" + writer
+				+ ", writeDate=" + writeDate + ", hit=" + hit + "]";
+	}
+	
+}
